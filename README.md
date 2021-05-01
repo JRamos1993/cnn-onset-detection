43083ba91ee13f38d3745a667910693de0b91561
Joao <jramos_93@icloud.com>
2021-05-01 20:48:44 +0100

    Improvements. Readme++.

--- README.md
+++ README.md
@@ -9,6 +9,14 @@ https://drive.google.com/file/d/0B-MfhNTg9c5eeDktWUpjeUF3VkU/view?usp=sharing
 
 There are more than 321 audio files and annotations.
 
+#### CNN Architecture
+Input shape 15x80x3
+... TODO
+
+#### Wavelet transform pre-processing
+Each audio from the dataset is split into frames of ±93ms with no hopping.
+A CWT is then applied to each of those frames, and an RGB scaleogram of size 15x80 is generated and stored into `dataset_transformed` folder.
+
 #### Folder structure
     .
     ├── dataset                     # Contains the Böck Dataset structure
@@ -17,14 +25,17 @@ There are more than 321 audio files and annotations.
     │   └── splits                  # Splits to use for cross-validation
     ├── dataset_transformed         # Contains the scaleograms that were converted from the audio files
     ├── logs                        # Contains logs for Tensorboard
-    ├── models                      # Contains the created kera model(s)
-    └── ...
+    ├── models                      # Contains the created keras model(s)
+    ├── pre-process.py              # Script to generate scaleograms from audio
+    └── train.py                    # Script to train with cross-validation and evaluate the CNN model with the scaleograms
 
 #### How to use
-1) Install requirements by running: `pip install -r requirements`
-2) Pre-process ...
+1) Install all the required packages by running: `pip install -r requirements`.
+2) Run the pre-processing script: `python pre-process.py`.
+3) Run the training script: `python train.py --epochs=10`.
 
 #### TODO's
+- Make possible to run training for less than 8 folds.
 - Refactor code into functions.
 - Make smaller hop-size between frames possible.
 - It should be possible to define frame sizes.
--- train.py
+++ train.py
@@ -1,92 +1,210 @@
 import numpy as np
 from os import makedirs
 from os import listdir
-from shutil import copyfile
-from random import seed
-from random import random
-from keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import SGD, Adam, Adadelta
-from tensorflow.keras.utils import Sequence
 from tensorflow.keras.layers import *
 from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
 import os
-import pandas as pd
-from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
-from sklearn.model_selection import KFold
+from os.path import basename, join
+from PIL import Image
+from tensorflow import keras
 import tensorflow as tf 
+from argparse import ArgumentParser
 
-# Some constants
-image_width = 80
-image_height = 15
-image_size = (image_width, image_height)
-image_channels = 3
-batch_size = 15
+def load_split_data(split_file):
+    split = np.loadtxt(split_file, dtype=str)
 
-# this is the augmentation configuration we will use for training
-train_datagen = ImageDataGenerator(
-    rescale = 1./255,
-    shear_range = 0.2,
-    zoom_range = 0.2
-)
+    X_train = [] # spectograms
+    Y_train = [] # labels
 
-# This is the augmentation configuration we will use for testing: only rescaling for now
-test_datagen = ImageDataGenerator(rescale = 1./255)
+    X_validation = [] # spectograms
+    Y_validation = [] # labels
 
-# This is a generator that will read pictures found in subfolers of 'data/train', and generate batches of augmented image data
-train_generator = train_datagen.flow_from_directory(
-    'dataset_transformed/train',
-    target_size = image_size,
-    batch_size = batch_size,
-    class_mode = 'binary'
-)
+    # Load all spectograms/labels into train and validation data structures
+    dataset = [join('dataset_transformed', f) for f in sorted(listdir('dataset_transformed'))]
+    for sample_path in dataset:
+        sample_file_name_with_extension = basename(sample_path)
+        sample_file_name = sample_file_name_with_extension.split('.')[0]
+        label = sample_file_name.split('-')[0] # First character is either 1 or 0 => onset or not onset
+        sample_name_clean = sample_file_name.split('-')[1]
 
-# This is a similar generator, for validation data
-validation_generator = test_datagen.flow_from_directory('dataset_transformed/validation', target_size = image_size, batch_size = batch_size, class_mode = 'binary')
+        image = Image.open(sample_path)
+        np_image = np.array(image)
 
-test_generator = test_datagen.flow_from_directory('dataset_transformed/test', target_size = image_size, batch_size = batch_size, class_mode = 'binary')
+        if sample_name_clean in split:
+            X_validation.append(np_image)
+            Y_validation.append(label)
+        else:
+            X_train.append(np_image)
+            Y_train.append(label)
 
-# input_shape = image_width,image_height,image_channels
-model = Sequential()
-model.add(Conv2D(10, (7, 3), input_shape = (image_width, image_height, image_channels), padding = 'same', activation = 'relu', data_format='channels_last'))
-model.add(MaxPooling2D(pool_size = (1, 3)))
-model.add(Conv2D(20, (3, 3), input_shape = (9, 26, 10), padding = 'valid', activation = 'relu'))
-model.add(MaxPooling2D(pool_size = (1, 3)))
-model.add(Dropout(0.5))
-model.add(Flatten())
-model.add(Dense(256, activation = 'sigmoid'))
-model.add(Dense(1, activation = 'sigmoid'))
+    # Post process
+    X_train = np.array(X_train)
+    X_validation = np.array(X_validation)
+    X_train = X_train.astype('float32') / 255.
+    X_validation = X_validation.astype('float32') / 255.
 
-optimizer = SGD(lr = 0.01, momentum = 0.9, clipvalue = 5)
+    Y_train = np.array(Y_train, dtype=int)
+    Y_validation = np.array(Y_validation, dtype=int)
 
-model.compile(loss = 'binary_crossentropy',
-    optimizer = optimizer,
-    metrics = [
-        tf.keras.metrics.BinaryAccuracy(),
-        tf.keras.metrics.FalseNegatives(),
-        tf.keras.metrics.FalsePositives(),
-        tf.keras.metrics.TrueNegatives(),
-        tf.keras.metrics.TruePositives(),
-    ]
-)
+    # Y_train = keras.utils.to_categorical(Y_train, 2)
+    # Y_validation = keras.utils.to_categorical(Y_validation, 2)
 
-mca = ModelCheckpoint('models/model_{epoch:03d}.h5', monitor = 'loss', save_best_only = False)
-mcb = ModelCheckpoint('models/model_best.h5', monitor = 'loss', save_best_only = True)
-mcv = ModelCheckpoint('models/model_best_val.h5', monitor = 'val_loss', save_best_only = True)
-es = EarlyStopping(monitor = 'val_loss', min_delta = 1e-4, patience = 20, verbose = True)
-tb = TensorBoard(log_dir = 'logs', write_graph = True, write_images = True)
-callbacks = [mca, mcb, mcv, es, tb]
+    return X_train, Y_train, X_validation, Y_validation
 
-history = model.fit(
-    train_generator, 
-    epochs = 50,
-    validation_data = validation_generator,
-    validation_steps = 584 // batch_size,
-    steps_per_epoch = 1611 // batch_size,
-    callbacks = callbacks
-)
+def get_model():
+    # Define model
+    model = Sequential()
+    model.add(Conv2D(10, (7, 3), input_shape = (15,80,3), padding = 'same', activation = 'relu', data_format='channels_last'))
+    model.add(MaxPooling2D(pool_size = (1, 3)))
+    model.add(Conv2D(20, (3, 3), input_shape = (9, 26, 10), padding = 'valid', activation = 'relu'))
+    model.add(MaxPooling2D(pool_size = (1, 3)))
+    model.add(Dropout(0.5))
+    model.add(Flatten())
+    model.add(Dense(256, activation = 'sigmoid'))
+    model.add(Dense(1, activation = 'sigmoid'))
 
-# Generate generalization metrics
-score = model.evaluate(test_generator, batch_size=batch_size)
-print(f'Test loss: {score[0]} / Test accuracy: {score[1]}')
+    optimizer = SGD(lr = 0.01, momentum = 0.9, clipvalue = 5)
+
+    model.compile(loss = 'binary_crossentropy',
+        optimizer = optimizer,
+        metrics = [
+            tf.keras.metrics.BinaryAccuracy(),
+            tf.keras.metrics.Precision(),
+            tf.keras.metrics.Recall(),
+            tf.keras.metrics.FalseNegatives(),
+            tf.keras.metrics.FalsePositives(),
+            tf.keras.metrics.TrueNegatives(),
+            tf.keras.metrics.TruePositives(),
+        ]
+    )
+
+    # print(model.summary())
+
+    return model
+
+def get_callbacks():
+    mca = ModelCheckpoint('models/model_{epoch:03d}.h5', monitor = 'loss', save_best_only = False)
+    mcb = ModelCheckpoint('models/model_best.h5', monitor = 'loss', save_best_only = True)
+    mcv = ModelCheckpoint('models/model_best_val.h5', monitor = 'val_loss', save_best_only = True)
+    es = EarlyStopping(monitor = 'val_loss', min_delta = 1e-4, patience = 20, verbose = False)
+    tb = TensorBoard(log_dir = 'logs', write_graph = True, write_images = True)
+
+    callbacks = [mca, mcb, mcv, es, tb]
+
+    return callbacks
+
+def train_fold(fold_number, X_train, Y_train, X_validation, Y_validation, epochs):
+    # Generate a print
+    print('------------------------------------------------------------------------')
+    print(f'Training for fold {fold_number} ...')
+
+    model = get_model()
+
+    history = model.fit(X_train, Y_train,
+        steps_per_epoch = len(X_train),
+        # initial_epoch = initial_epoch,
+        epochs = epochs,
+        validation_data = (X_validation, Y_validation),
+        validation_steps = len(X_validation),
+        # callbacks = get_callbacks(),
+    )
+
+    return history.history
+
+def evaluate_folds(scores):
+    loss = []
+    binary_accuracy = []
+    precision = []
+    recall = []
+
+    tp = []
+    tn = []
+    fp = []
+    fn = []
+
+    a = 0
+    for score in scores:
+        loss.append(np.mean(score['loss']))
+        binary_accuracy.append(np.mean(score['binary_accuracy']))
+
+        if a == 0:
+            precision.append(np.mean(score['precision']))
+            recall.append(np.mean(score['recall']))
+
+            tp.append(np.mean(score['true_positives']))
+            tn.append(np.mean(score['true_negatives']))
+            fp.append(np.mean(score['false_positives']))
+            fn.append(np.mean(score['false_negatives']))
+        else:
+            precision.append(np.mean(score['precision_' + str(a)]))
+            recall.append(np.mean(score['recall_' + str(a)]))
+
+            tp.append(np.mean(score['true_positives_' + str(a)]))
+            tn.append(np.mean(score['true_negatives_' + str(a)]))
+            fp.append(np.mean(score['false_positives_' + str(a)]))
+            fn.append(np.mean(score['false_negatives_' + str(a)]))
+        a += 1
+
+    prec = np.mean(precision)
+    rec = np.mean(recall)
+    f_measure = 2 * prec * rec / (prec + rec)
+
+    tp = np.mean(tp)
+    tn = np.mean(tn)
+    fp = np.mean(fp)
+    fn = np.mean(fn)
+
+    print('------------------------------------------------------------------------')
+    print('Average scores for all folds:')
+    print(f'> Accuracy: {np.mean(binary_accuracy)} (+- {np.std(binary_accuracy)})')
+    print(f'> Loss: {np.mean(loss)}')
+    print(f'> Precision: {prec}')
+    print(f'> Recall: {rec}')
+    print(f'> F-Measure: {f_measure}')
+    print(f'> True Positives: {round(tp)} True Negatives: {round(tn)}')
+    print(f'> False Positives: {round(fp)} False Negatives: {round(fn)}')
+    print('------------------------------------------------------------------------')
+
+def main():
+    # Argument parsing
+    parser = ArgumentParser(description = 'Onset Detection Trainer')
+    parser.add_argument(
+        '-e', '--epochs',
+        type = int,
+        default = 5,
+        help = 'number of epochs to train each model for')
+    args = parser.parse_args()
+
+    # TODO use this
+    req_action.add_argument(
+        '-t', '--train',
+        type = int,
+        help = 'number of models to train, trained and validated with different folds of the same data')
+
+    # Load splits information
+    splits_dir = join('dataset', 'splits')
+    splits_files = [join(splits_dir, f) for f in sorted(listdir(splits_dir))]
+
+    # Some constants
+    scores = []
+    fold_number = 1
+
+    for split_file in splits_files:
+        # Load all images associated with the fold
+        (X_train, Y_train, X_validation, Y_validation) = load_split_data(split_file)
+
+        # Train the fold
+        results = train_fold(fold_number, X_train, Y_train, X_validation, Y_validation, args.epochs)
+
+        # Save results to array
+        scores.append(results)
+            
+        fold_number += 1
+
+    # Evalute and print all results
+    evaluate_folds(scores)
+
+if __name__ == '__main__':
+    main()
